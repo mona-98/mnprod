419f3fe9816edefe54635802805946dc0067a086
niviten <l.j.nivethan@gmail.com>
2020-12-31 23:06:05 +0530

    init

---
+++ app/Views/templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>mnprod</title>
+    </head>
+    <body>
+        <h1>
+            <?= esc($title); ?>
+        </h1>
